a97b499e86cc1827b469329c0faa571ce0de0164
Ederson Machado <edersonmberti@gmail.com>
2019-11-06 18:01:41 -0300

    add optional config object

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import TouchID from 'react-native-touch-id';
 
 export default function App() {
   function onAuthTouchId() {
-    TouchID.authenticate('to demo this react-native component')
+    TouchID.authenticate(
+      'to demo this react-native component',
+      optionalConfigObject,
+    )
       .then(success => {
         alert('Authenticated Successfully');
       })
@@ -13,8 +16,35 @@ export default function App() {
       });
   }
 
+  const optionalConfigObject = {
+    title: 'Authentication Required', // Android
+    imageColor: '#808080', // Android
+    imageErrorColor: '#ff0000', // Android
+    sensorDescription: 'Touch sensor', // Android
+    sensorErrorDescription: 'Failed', // Android
+    cancelText: 'Cancel', // Android
+    fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
+    unifiedErrors: false, // use unified error messages (default false)
+    passcodeFallback: true, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
+  };
+
+  function supported() {
+    TouchID.isSupported(optionalConfigObject)
+      .then(biometryType => {
+        if (biometryType === 'FaceID') {
+          alert('FaceID is supported.');
+        } else {
+          alert('TouchID is supported.');
+        }
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+
   return (
     <View style={styles.container}>
+      {supported()}
       <TouchableOpacity onPress={onAuthTouchId}>
         <Text>Auth with Touch ID</Text>
       </TouchableOpacity>
